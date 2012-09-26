0946e7e7b0a6f76fe67ec797116a128a203bffd9
MMiszy <lupinek7@gmail.com>
2012-09-26 22:27:57 +0200

    Add background renderer

--- main.lua
+++ main.lua
@@ -1,90 +1,60 @@
--- Abstract: MultiPuck sample project
--- Demonstrates multitouch and draggable phyics objects using "touch joints" in gameUI library
---
--- Version: 1.1
--- 
--- Sample code is MIT licensed, see http://www.coronalabs.com/links/code/license
--- Copyright (C) 2010 Corona Labs Inc. All Rights Reserved.
---
--- History
---  1.0		9/1/10		Initial version
---	1.1		5/13/11		Fixed bug with stuck pucks. Added "return" to dragBody()
---						Added "return true" to spawnDisk()
---  1.2     6/28/11     Call table.remove in removeOffscreenItems to keep allDisks as an array
--------------------------------------------------------------------------------------
+local common = require("common")
 
-display.setStatusBar( display.HiddenStatusBar )
+local function drawTile (t, left, top, width, height)
+	local container = display.newGroup()
+	local bg = display.newRect(left, top, width, height)
+	bg.strokeWidth = 1
+	bg:setFillColor(255,0,0)
 
-local physics = require("physics")
-local gameUI = require("gameUI")
-local easingx  = require("easingx")
+	container:insert(bg)
 
-physics.start()
-physics.setGravity( 0, 0 ) -- no gravity in any direction
 
-popSound = audio.loadSound ("pop2_wav.wav")
-labelFont = gameUI.newFontXP{ ios="Zapfino", android=native.systemFont }
+	local miniTileWidth = width/3
+	local miniTileHeight = height/3
 
-system.activate( "multitouch" )
+	if (t.t) then
+		local tile = display.newRect(left+miniTileWidth, top, miniTileWidth, miniTileHeight)
+		tile:setFillColor(0,255,0)
+		container:insert(tile)
+	end
+	if (t.b) then
+		local tile = display.newRect(left+miniTileWidth, top+height-miniTileHeight, miniTileWidth, miniTileHeight)
+		tile:setFillColor(0,255,0)
+		container:insert(tile)
+	end
+	if (t.l) then
+		local tile = display.newRect(left, top+miniTileHeight, miniTileWidth, miniTileHeight)
+		tile:setFillColor(0,255,0)
+		container:insert(tile)
+	end
+	if (t.r) then
+		local tile = display.newRect(left+width-miniTileWidth, top+miniTileHeight, miniTileWidth, miniTileHeight)
+		tile:setFillColor(0,255,0)
+		container:insert(tile)
+	end
 
-local bkg = display.newImage( "paper_bkg.png", true )
-bkg.x = display.contentWidth/2
-bkg.y = display.contentHeight/2
+	return container
+end
 
-local myLabel = display.newText( "Touch screen to create pucks", 0, 0, labelFont, 34 )
-myLabel:setTextColor( 255, 255, 255, 180 )
-myLabel.x = display.contentWidth/2
-myLabel.y = 200
+local function initGame (settings)
+	local tiles = {}
 
-local diskGfx = { "puck_yellow.png", "puck_green.png", "puck_red.png" }
-local allDisks = {} -- empty table for storing objects
 
--- Automatic culling of offscreen objects
-local function removeOffscreenItems()
-	for i = 1, #allDisks do
-		local oneDisk = allDisks[i]
-		if (oneDisk and oneDisk.x) then
-			if oneDisk.x < -100 or oneDisk.x > display.contentWidth + 100 or oneDisk.y < -100 or oneDisk.y > display.contentHeight + 100 then
-				oneDisk:removeSelf()
-                table.remove( allDisks, i ) 
- 			end	
+	local tileWidth = display.contentWidth/settings.tilesX
+	local tileHeight = tileWidth
+
+
+	for y=1, settings.tilesY, 1 do
+		for x=1, settings.tilesX, 1 do
+			if (tiles[x] == nil) then tiles[x] = {} end
+			tiles[x][y] = drawTile({t = true, b = true, l = true, r = true}, (x-1)*tileWidth, (y-1)*tileHeight+25, tileWidth, tileHeight);
+			tiles[x][y]:addEventListener("touch", function (event)
+				if (event.phase == "began") then
+					print(event.target)
+				end
+			end)
 		end
 	end
 end
 
-local function dragBody( event )
-	return gameUI.dragBody( event )
-	
-	-- Substitute one of these lines for the line above to see what happens!
-	--gameUI.dragBody( event, { maxForce=400, frequency=5, dampingRatio=0.2 } ) -- slow, elastic dragging
-	--gameUI.dragBody( event, { maxForce=20000, frequency=1000, dampingRatio=1.0, center=true } ) -- very tight dragging, snaps to object center
-end
-
-local function spawnDisk( event )
-	local phase = event.phase
-	
-	if "ended" == phase then
-		audio.play( popSound )
-		myLabel.isVisible = false
-
-		randImage = diskGfx[ math.random( 1, 3 ) ]
-		allDisks[#allDisks + 1] = display.newImage( randImage )
-		local disk = allDisks[#allDisks]
-		disk.x = event.x; disk.y = event.y
-		disk.rotation = math.random( 1, 360 )
-		disk.xScale = 0.8; disk.yScale = 0.8
-		
-		transition.to(disk, { time = 500, xScale = 1.0, yScale = 1.0, transition = easingx.easeOutElastic }) -- "pop" animation
-		
-		physics.addBody( disk, { density=0.3, friction=0.6, radius=66.0 } )
-		disk.linearDamping = 0.4
-		disk.angularDamping = 0.6
-		
-		disk:addEventListener( "touch", dragBody ) -- make object draggable
-	end
-	
-	return true
-end
-
-bkg:addEventListener( "touch", spawnDisk ) -- touch the screen to create disks
-Runtime:addEventListener( "enterFrame", removeOffscreenItems ) -- clean up offscreen disks
+initGame({tilesX = 4, tilesY = 5})
